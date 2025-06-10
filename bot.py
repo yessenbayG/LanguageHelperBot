c49a4e3cd83b5a5a2f7f133a65932ecce2b6ebee
Galym <talgat2003tinishjan@gmail.com>
2025-06-10 09:48:58 +0300

    Add functions send_phrase and add_new_phrase

--- bot.py
+++ bot.py
@@ -28,8 +28,48 @@ cursor.executemany('INSERT OR IGNORE INTO phrases (word, translation, example) V
 conn.commit()
 
 
-#Функция для получения случайной фразы из базы данных
 def get_random_phrase():
-    cursor.execute('SELECT word, translation, example FROM phrases ORDER BY RANDOM() LIMIT LIMIT 1')
+    cursor.execute('SELECT word, translation, example FROM phrases ORDER BY RANDOM() LIMIT 1')
     return cursor.fetchone()
 
+
+def add_phrase(word, translation, example):
+    cursor.execute('INSERT INTO phrases (word, translation, example) VALUES (?, ?, ?)', (word, translation, example))
+    conn.commit()
+
+
+@bot.message_handler(commands=['start'])
+def send_welcome(message):
+    bot.send_message(message.chat.id,
+                     "Привет! Я LanguageHelperBot."
+                     "Отправь команду /phrase, чтобы получить случайное слово или фразу для изучения."
+                     "Отправь команду /add слово, перевод, пример, чтобы добавить новую фразу.")
+
+
+@bot.message_handler(commands=['phrase'])
+def send_phrase(message):
+    word, translation, example = get_random_phrase()
+
+    phrase_message = (f"Слово: {word}\n"
+                      f"Перевод: {translation}\n"
+                      f"Пример: {example}")
+    bot.send_message(message.chat.id, phrase_message)
+
+
+@bot.message_handler(commands=['add'])
+def add_new_phrase(message):
+    try:
+        # Удаляем команду /add и разбиваем оставшийся текст на части
+        parts = message.text[len('/add'):].split(',')
+        if len(parts) != 3:
+            raise ValueError("Неверное количество аргументов")
+
+        word, translation, example = map(str.strip, parts)
+
+        add_phrase(word, translation, example)
+        bot.send_message(message.chat.id, f"Фраза '{word}' успешно добавлена!")
+    except ValueError:
+        bot.send_message(message, "Пожалуйста, используйте формат: /add слово, перевод, пример")
+
+
+bot.infinity_polling()
